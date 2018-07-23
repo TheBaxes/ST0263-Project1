3ad902f948a3278547e9376532e33f1f4551ce1c
TheBaxes <sebas1475@gmail.com>
2018-07-22 21:11:25 -0500

    Finish prototipe

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :users  do
     resources :locations
   end
+
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+
+  root 'users#index'
 end
